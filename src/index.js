3a538af658b092e2fd68f33db88b38131f024cfb
Parisa Zamani <parisa.zmni1994@gmail.com>
2020-01-28 12:32:31 +0330

    add btn in start game

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 //Vendor
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-
-//Redux store
-import {store} from "./redux/store";
 
 //Router and start application flow
 import {App} from './App';
 
 //Styles loader
 import './assets/StyleLoader';
-
 ReactDOM.render(
-    <Provider store={store}>
         <App />
-    </Provider>
     , document.getElementById('root'));
 
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,57 @@
 import React from 'react';
+import { Container, Button } from 'reactstrap';
+import { is } from '@babel/types';
 class home extends React.Component {
-    state = {  }
-    render() { 
+    state = {  
+        scoreValue: 0,
+        deck :[],
+        rank: [2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"],
+        cardName: ["pick", "khesht", "del", "khaj" ],
+        isStart: false,
+    }
+
+    playGame = (e) => {
+        this.setState(prevState => ({isStart: !prevState.isStart}));
+        this.makeCard();
+    }
+
+    makeCard = () => {
+
+    }
+
+    highterGame = (e) => {
+        console.log("highter");
+    }
+
+    lowerGame = () => {
+        console.log("lower");
+    }
+
+    render() {
+        const { isStart } = this.state;
         return ( 
-            <div className="text-center">
-                <h1>text</h1>
-                <p>
-                    this is test
-                </p>
-            </div>
+            <Container className="text-center">
+                <h2>Score: <div className="score">0</div></h2>
+                <div className="message">
+                    {
+                        isStart ?
+                        'click in hight or lower'
+                        : 
+                        ' click start'
+                    }
+                </div>
+                <div className="gameplay"></div>
+                <Button type="button" className={`${isStart ? 'btn-hidden' : ''} ml-2 `} children={"Start"} onClick={this.playGame} />
+                {
+                    isStart && (
+                        <React.Fragment>
+                            <Button type="button" className="ml-2"children={"Highter"} onClick={this.highterGame} />
+                            <Button type="button" className="ml-2" children={"Lower"} onClick={this.lowerGame} />
+                        </React.Fragment>
+                    )
+                }
+                
+            </Container>
         );
     }
 }
